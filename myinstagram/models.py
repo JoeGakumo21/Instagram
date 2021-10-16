cbe25749d47c9caec51a852592b8f2f219075046
Joe Gakumo <joegakumo1@gmail.com>
2021-10-16 22:51:42 +0300

    add an image and display it

--- myinstagram/models.py
+++ myinstagram/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
     author=models.ForeignKey('auth.User',on_delete=models.CASCADE)
-    myimage=models.ImageField(blank=True, null=True)
+    image=models.ImageField(blank=True, null=True)
     mycaption=models.TextField()
     created_date=models.DateTimeField(default=timezone.now)
 
