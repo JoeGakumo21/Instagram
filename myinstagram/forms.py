f7dcbdab8e2a3b1e849bbc04412b665ae0819d2f
Joe Gakumo <joegakumo1@gmail.com>
2021-10-16 23:09:00 +0300

    create a postform class

--- myinstagram/forms.py
+++ myinstagram/forms.py
@@ -1,3 +1,23 @@
 from django import forms
+from django.db.models import fields
+
+from myinstagram.views import post
 from .models import Post
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout,Field
+
+from myinstagram import models
+
+
+class PostForm(forms.ModelForm):
+    helper=FormHelper()
+    helper.form_method="POST"
+    helper.add_input(Submit('Post','post', css_class='btn-primary'))
+
+    class Meta:
+        model=post
+        fields=[
+            'image',
+            'mycaption'
+        ]
