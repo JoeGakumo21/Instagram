025b00662d3c222b4323fab3d82195f47a7a0f8a
Joe Gakumo <joegakumo1@gmail.com>
2021-10-17 16:35:44 +0300

    add like and dislike classes

--- myinstagram/models.py
+++ myinstagram/models.py
@@ -17,3 +17,19 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     body=models.TextField()
     date= models.DateTimeField(auto_now_add=True)
+
+    
+
+LIKE_CHOICES = (
+    ('Like','Like'),
+    ('Unlike','Unlike'),
+)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOICES,default='Like',max_length=10)
+
+    def __str__(self):
+        return str(self.post)    
