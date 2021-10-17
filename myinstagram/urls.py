b912340434cea0b0f1f78d2299715073a95c1063
Joe Gakumo <joegakumo1@gmail.com>
2021-10-17 08:24:56 +0300

    creating login and signup templates and linked then

--- myinstagram/urls.py
+++ myinstagram/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+   path('register/', views.registerPage, name='register'),
+   path('login/', views.loginPage, name='login'),
    path('', views.post, name='post'),
-   path('newpost/', views.newpost, name='newpost'),
+   path('newpost/', views.NewPost, name='newpost'),
+
 ]
--- myinstagram/views.py
+++ myinstagram/views.py
@@ -1,33 +1,35 @@
 
 from django.shortcuts import render,redirect
 from .models import Post
-from django.forms import NewPostForm
 def post(request):
     posts = Post.objects.all().filter
     return render(request,'all-in-one/post.html',{'posts':posts})
 
+def loginPage(request):
+    context={}
+    return render(request,'all-in-one/login.html', context)
+
+def registerPage(request):
+    context={}
+    return render(request,'all-in-one/register.html', context)
+
 def NewPost(request):
-    tags_objs=[]
-    if request.method=="POST":
-        form=NewPostForm(request.POST, request.FILES)
+    posts = Post.objects.all()
 
-        if form.is_valid():
-            image=form.cleaned_data.get('image')
-            mycaption=form.cleaned_data.get('mycaption')
-            tags_form=form.cleaned_data.get('tags')
-            tags_list = list(tags_form.split(','))
-
-            for tag in tags_list:
-                t, created=tag.objects.get_or_create(title=tag)
-                tags_objs.append(t)
-                p, created=Post.objects.get_or_create(image=image, mycaption=mycaption)
-                p.tags.set(tags_objs)
-                p.save()
-                return  redirect('post')
-
-    else:
-        form=NewPostForm()
-    context={
-        'form':form
-    }  
-    return render(request, 'all-in-one/newpost.html', context)  
+    if request.method =='POST':
+        data=request.POST
+        image=request.FILES.get('image')
+        if data['posts'] != 'none':
+            posts= Post.objects.get(id=data['category']) 
+        elif data['newpost'] != '':
+            posts,created=Post.objects.get_or_create(name=data['newpost'])
+        else:
+            posts=None    
+        photo=image.objects.create(
+            image=image,
+            mycaption=data['mycaption'],
+            
+        ) 
+        return redirect('post')
+    context={'posts':posts}
+    return render(request, 'all-in-one/newpost.html', context)     
