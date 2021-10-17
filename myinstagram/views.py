d19a9ddcc10a006185411285d011fb2e5e8d8440
Joe Gakumo <joegakumo1@gmail.com>
2021-10-17 07:18:11 +0300

    created newpostform in viewse

--- myinstagram/views.py
+++ myinstagram/views.py
@@ -1,8 +1,17 @@
 
 from django.shortcuts import render
 from .models import Post
+from django.forms import NewPostForm
 def post(request):
     posts = Post.objects.all().filter
     return render(request,'all-in-one/post.html',{'posts':posts})
 
-    
+def NewPost(request):
+    tags_objs=[]
+    if request.method=="POST":
+        form=NewPostForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            image=form.cleaned_data.get('image')
+            mycaption=form.cleaned_data.get('mycaption')
+            tags_form=form.cleaned_data.get('tags')
