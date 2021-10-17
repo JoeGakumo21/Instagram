bb64b03bc9757cbdbe26dc81cde24645abadeea0
Joe Gakumo <joegakumo1@gmail.com>
2021-10-17 07:32:28 +0300

    add functionality to allow use of forme

--- myinstagram/views.py
+++ myinstagram/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post
 from django.forms import NewPostForm
 def post(request):
@@ -15,3 +15,19 @@ def NewPost(request):
             image=form.cleaned_data.get('image')
             mycaption=form.cleaned_data.get('mycaption')
             tags_form=form.cleaned_data.get('tags')
+            tags_list = list(tags_form.split(','))
+
+            for tag in tags_list:
+                t, created=tag.objects.get_or_create(title=tag)
+                tags_objs.append(t)
+                p, created=Post.objects.get_or_create(image=image, mycaption=mycaption)
+                p.tags.set(tags_objs)
+                p.save()
+                return  redirect('post')
+
+    else:
+        form=NewPostForm()
+    context={
+        'form':form
+    }  
+    return render(request, 'all-in-one/newpost.html', context)  
