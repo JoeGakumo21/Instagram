e4300ecb243360e58c70073bce14143a2e12ea07
Joe Gakumo <joegakumo1@gmail.com>
2021-10-18 11:58:20 +0300

    minor charges

--- myinstagram/models.py
+++ myinstagram/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+
 # Create your models here.
 class Post(models.Model):
     author=models.ForeignKey('auth.User',on_delete=models.CASCADE)
